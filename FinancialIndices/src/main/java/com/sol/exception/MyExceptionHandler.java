dc3f5a01de909c467e63097bb61cc587b82e0a5e
Manish Madhusoodhan <manish.mrao@gmail.com>
2021-02-19 16:52:38 +0530

    Update MyExceptionHandler.java

--- FinancialIndices/src/main/java/com/sol/exception/MyExceptionHandler.java
+++ FinancialIndices/src/main/java/com/sol/exception/MyExceptionHandler.java
@@ -1,9 +1,9 @@
 package com.sol.exception;
 
 import java.util.HashMap;
+
 import java.util.Map;
 
-import javax.validation.constraints.NotNull;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,37 +16,22 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 
 import com.fasterxml.jackson.core.JsonParseException;
+
 @ControllerAdvice
 public class MyExceptionHandler {
-		/*
-		 * @ExceptionHandler(EntityNotFoundException.class) public final
-		 * ResponseEntity<ExceptionMessage>
-		 * EntityNotFoundException(EntityNotFoundException ex, WebRequest request) {
-		 * ExceptionMessage error = new ExceptionMessage("Not found", ex.getMessage());
-		 * return new ResponseEntity<ExceptionMessage>(error, HttpStatus.BAD_REQUEST); }
-		 */
-	    
-	   
-	    
-		/*
-		 * @ExceptionHandler(NumberFormatException.class) public final
-		 * ResponseEntity<ExceptionMessage> NumberFormatException(NumberFormatException
-		 * ex, WebRequest request) { ExceptionMessage error = new
-		 * ExceptionMessage("Not a valid number", ex.getMessage()); return new
-		 * ResponseEntity<ExceptionMessage>(error, HttpStatus.BAD_REQUEST); }
-		 */
+		
 	    
 	    @ExceptionHandler(JsonParseException.class)
 	    public final ResponseEntity<ExceptionMessage> JsonParserException(JsonParseException ex, WebRequest request) {
 	        ExceptionMessage error = new ExceptionMessage("Error parsing JSON", ex.getMessage());
-	        return new ResponseEntity<ExceptionMessage>(error, HttpStatus.BAD_REQUEST);
+	        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
 	    }
 	    
 
 	    @ExceptionHandler(ConstraintViolationException.class)
-	    public final ResponseEntity<ExceptionMessage> Constra(ConstraintViolationException ex, WebRequest request) {
+	    public final ResponseEntity<ExceptionMessage> ConstraintViolationException(ConstraintViolationException ex, WebRequest request) {
 	        ExceptionMessage error = new ExceptionMessage("Please check input values", ex.getMessage());
-	        return new ResponseEntity<ExceptionMessage>(error, HttpStatus.BAD_REQUEST);
+	        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
 	    }
 	    
 	    @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -62,9 +47,6 @@ public class MyExceptionHandler {
 	        }
 
 	        return errors;
-	    }
-	    
-	    
-		
+	    }	
 	
 }
